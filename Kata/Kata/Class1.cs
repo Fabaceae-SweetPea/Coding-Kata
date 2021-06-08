b214fb5420b3697dec718de0b7e2cbc084d0b539
Fabaceae-SweetPea <85483584+Fabaceae-SweetPea@users.noreply.github.com>
2021-06-08 11:11:51 +0100

    Hardcoded values
    Hard coded values

--- Kata/Kata/Class1.cs
+++ Kata/Kata/Class1.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Net;
 
 namespace Kata
 {
     public class Checkout
     {
         public static List<Item> basket = new List<Item>();
+        public static double total = 0;
 
         public static double Total()
         {
-            double total = 0;
-
             if (basket.Count > 0)
             {
                 foreach (Item i in basket)
@@ -23,10 +23,51 @@ namespace Kata
             return total;
         }
 
+        public static void ApplyDiscount()
+        {
+            int itemCountB15 = 0;
+            int itemCountA99 = 0;
+
+            foreach (Item i in basket)
+            {
+                if (i.SKU == "B15")
+                {
+                    itemCountB15++;
+                }
+
+                if (i.SKU == "A99")
+                {
+                    itemCountA99++;
+                }
+            }
+            
+            if (itemCountB15 % 2 >= 1)
+            {
+                int discountNumber = itemCountB15 / 2;
+                total += 0.45 * discountNumber;
+                total -= 0.30 * (discountNumber * 2);
+            }
+
+            if (itemCountA99 % 3 >= 1)
+            {
+                int discountNumber = itemCountA99 / 3;
+                total += 1.30 * discountNumber;
+                total -= 0.50 * (discountNumber * 3);
+            }
+        }
+
         public static void Scan(Item item)
         {
-            basket.Add(item);
+            if (item.SKU == "A99" || item.SKU == "B15" || item.SKU == "C40")
+            {
+                basket.Add(item);
+            }
+            else
+            {
+                new ArgumentException("Item does not exist");
+            }     
         }
+
     }
 
     public class Item
--- Kata/KataTest/CheckoutTest.cs
+++ Kata/KataTest/CheckoutTest.cs
@@ -14,10 +14,19 @@ namespace KataTest
         [Test]
         public void ScanKnownItem()
         {
-            Item firstItem = new Item("A99", 0.50);
+            Item item1 = new Item("A99", 0.50);
+            Item item2 = new Item("B15", 0.30);
+            Item item3 = new Item("C40", 0.60);
 
-            Checkout.Scan(firstItem);
-
+            Checkout.Scan(item1);
+            Checkout.Scan(item1);
+            Checkout.Scan(item1);
+            Checkout.Scan(item1);
+            Checkout.Scan(item1);
+            Checkout.Scan(item2);
+            Checkout.Scan(item2);
+            Checkout.Scan(item2);
+            Checkout.Scan(item3);
         }
 
         [Test]
@@ -30,6 +39,7 @@ namespace KataTest
         public void TotalPrice()
         {
             Checkout.Total();
+            Checkout.ApplyDiscount();
         }
 
     }
